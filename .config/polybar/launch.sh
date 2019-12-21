40b720961b06820a097f838c8e1077525fde363b
UniverseSquared <universesquared4@gmail.com>
2019-12-21 17:06:43 +0000

    polybar: add launch script

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# kill polybar if it's already running
+killall -q polybar
+
+# wait until it dies
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# launch polybar
+polybar mybar &
+
+# lower polybar below other windows
+sleep 1
+xdo lower -p $(pidof polybar)
