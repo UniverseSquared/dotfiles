26df1f9f81002fa89d4e810511e62da31671eaa1
UniverseSquared <universesquared4@gmail.com>
2020-12-13 12:50:42 +0000

    polybar: start polybar on every available monitor

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,5 +6,7 @@ killall -q polybar
 # wait until it dies
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# launch polybar
-polybar -r mybar &
+# launch polybar on each monitor
+for monitor in $(polybar -m | cut -d ':' -f 1); do
+    MONITOR=$monitor polybar -r mybar &
+done
