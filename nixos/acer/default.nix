2056ff5aee12e7afc1bd569801ccc7da84d8e938
UniverseSquared <universesquared4@gmail.com>
2025-03-19 20:41:03 +0000

    disable rgb at startup

--- nixos/acer/default.nix
+++ nixos/acer/default.nix
@@ -1,5 +1,8 @@
 { config, pkgs, ... }:
 
+let
+  facer-rgb = pkgs.callPackage ./facer-rgb.nix { };
+in
 {
   boot.extraModulePackages = [
     (config.boot.kernelPackages.callPackage ./rgb.nix { })
@@ -7,7 +10,15 @@
 
   boot.kernelModules = [ "facer" ];
 
-  environment.systemPackages = [
-    (pkgs.callPackage ./facer-rgb.nix { })
-  ];
+  environment.systemPackages = [ facer-rgb ];
+
+  # turn off rgb at startup
+  systemd.services.acer-rgb = {
+    enable = true;
+    script = ''
+      ${facer-rgb}/bin/facer-rgb -b 0
+    '';
+
+    wantedBy = [ "multi-user.target" ];
+  };
 }
