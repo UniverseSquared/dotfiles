0a6a039e26b496e4bbbf65c0731d7576e7f25173
UniverseSquared <universesquared4@gmail.com>
2025-03-27 18:56:32 +0000

    firefox: blank new tab page

--- home/firefox.nix
+++ home/firefox.nix
@@ -19,6 +19,14 @@
         "browser.aboutConfig.showWarning" = false;
         "general.autoScroll" = true; # enable middle click scrolling
         "ui.key.menuAccessKeyFocuses" = false; # don't summon the menu bar with alt
+        "browser.toolbars.bookmarks.visibility" = "never";
+
+        # restore previous tabs on startup
+        "browser.startup.page" = 3;
+
+        # remove search bar and stuff from new tab page
+        "browser.newtabpage.activity-stream.feeds.topsites" = false;
+        "browser.newtabpage.activity-stream.showSearch" = false;
       };
     };
   };
