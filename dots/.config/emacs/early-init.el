fdc918b8e5f9738cc79eaafb3f7e96963a051ed8
UniverseSquared <universesquared4@gmail.com>
2025-07-29 20:43:56 +0100

    delete old configuration files

--- dots/.config/emacs/early-init.el
+++
@@ -1,4 +0,0 @@
-;; Disable unnecessary GUI elements, before the UI is initialized (early-init.el)
-(push '(menu-bar-lines . 0) default-frame-alist)
-(push '(tool-bar-lines . 0) default-frame-alist)
-(push '(vertical-scroll-bars) default-frame-alist)
