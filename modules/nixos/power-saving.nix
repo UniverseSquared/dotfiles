855a69b5eb798dc06032d2873110e6d1003d67ed
UniverseSquared <universesquared4@gmail.com>
2026-07-02 16:36:07 +0100

    remove mobile specialisation

--- modules/nixos/power-saving.nix
+++ modules/nixos/power-saving.nix
@@ -7,31 +7,12 @@
     # enable power management for nvidia gpu per https://wiki.archlinux.org/title/TLP#PRIME_with_NVIDIA_driver
     # maybe also set RUNTIME_PM_ON_AC=auto for power management on ac too? (see https://linrunner.de/tlp/support/optimizing.html#opt-reduce-power-on-ac)
     # i think there are three profiles to use in tlp, maybe default to balanced profile on ac (where the default is performance) and have a script to change to performance when necessary (then don't need to use cpupower or whatever to change governor as well)
-    settings.RUNTIME_PM_ENABLE = "01:00.0";
-  };
+    settings = {
+      TLP_DEFAULT_MODE = "BAL";
+      TLP_AUTO_SWITCH = 0;
 
-  specialisation.mobile.configuration = {
-    system.nixos.tags = [ "mobile" ];
-
-    boot.blacklistedKernelModules = [
-      "nvidia"
-      "nouveau"
-    ];
-
-    services.xserver.videoDrivers = lib.mkForce [ ];
-
-    hardware.nvidia = {
-      modesetting.enable = lib.mkForce false;
-
-      powerManagement = {
-        enable = lib.mkForce false;
-        finegrained = lib.mkForce false;
-      };
-
-      prime.offload = {
-        enable = lib.mkForce false;
-        enableOffloadCmd = lib.mkForce false;
-      };
+      RUNTIME_PM_ENABLE_ON_AC = "auto";
+      RUNTIME_PM_ENABLE = "01:00.0"; # autosuspend nvidia gpu
     };
   };
 }
