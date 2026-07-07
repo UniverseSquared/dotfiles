58ba80cf4d66fdc50a89a359979e20ee78dbb47d
UniverseSquared <universesquared4@gmail.com>
2026-07-07 11:55:51 +0100

    syncthing: don't sync org stuff with waso

--- hosts/kala/syncthing.nix
+++ hosts/kala/syncthing.nix
@@ -1,7 +1,11 @@
 {
   dawson.syncthing.folders = {
+    org = {
+      path = "~/org";
+      devices = [ "phone" ];
+    };
+
     cemu.path = "~/.local/share/Cemu/mlc01/usr/save";
-    org.path = "~/org";
     yuzu-system.path = "~/Sync/yuzu-system";
     yuzu-user.path = "~/Sync/yuzu-user";
     dolphin-wii.path = "~/Sync/dolphin-wii";
